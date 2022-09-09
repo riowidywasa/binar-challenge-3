fb49aac6d49d676ffd0df2a230c1f69f0fe92a71
riowidywasa <riowidywasa@gmail.com>
2022-09-09 15:20:42 +0700

    [wdyws27] add function bubbleShort by descending

--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -1,8 +1,8 @@
 function sortCarByYearDescendingly(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
   // console.log(cars);
-  console.log("Sebelum diurutkan dari yang tahun yang terbesar")
-  console.table(cars)
+  console.log("Sebelum diurutkan dari yang tahun yang terbesar");
+  console.table(cars);
   // Clone array untuk menghindari side-effect
   // Apa itu side effect?
   const result = [...cars];
@@ -10,8 +10,8 @@ function sortCarByYearDescendingly(cars) {
   // Tulis code-mu disini
     for (let i = 0; i < result.length - 1; i++) {
         for (let ii = 0; ii < result.length - 1; ii++) {
-            now = result[ii].year
-            next = result[ii+1].year
+            now = result[ii].year;
+            next = result[ii+1].year;
             if(now < next){
                 temp = result[ii];
                 result[ii] = result[ii+1];
@@ -21,9 +21,9 @@ function sortCarByYearDescendingly(cars) {
     }
 
   // Rubah code ini dengan array hasil sorting secara ascending
-    console.log("Sesudah diurutkan")
-    console.table(result)
-    return result
+    console.log("Sesudah diurutkan");
+    console.table(result);
+    return result;
 }
 
 
