66599d2d1cddce9224abfea6fd5a438ddefb996a
riowidywasa <riowidywasa@gmail.com>
2022-09-09 15:13:58 +0700

    [wdyws27] no change

---
+++ scripts/filterCarByAvailability.js
@@ -0,0 +1,19 @@
+function filterCarByAvailability(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log("Sebelum Filter")
+  console.table(cars);
+
+  // Tempat penampungan hasil
+  const result = [];
+
+  // Tulis code-mu disini
+  for(let i=0; i < cars.length-1;i++){
+    if(cars[i].available === true){
+      result.push(cars[i]);
+    }
+  }
+  console.log("Sesuadah Filter");
+  console.table(result);
+  // Rubah code ini dengan array hasil filter berdasarkan availablity
+  return result;
+}
---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,27 @@
+function sortCarByYearAscendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  // console.log(cars);
+  console.log("Sebelum diurutkan dari yang tahun yang terkecil")
+  console.table(cars)
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+    for (let i = 0; i < result.length - 1; i++) {
+        for (let ii = 0; ii < result.length - 1; ii++) {
+            now = result[ii].year
+            next = result[ii+1].year
+            if(now > next){
+                temp = result[ii];
+                result[ii] = result[ii+1];
+                result[ii+1]= temp;                    
+            }
+        }
+    }
+
+  // Rubah code ini dengan array hasil sorting secara ascending
+    console.log("Sesudah diurutkan");
+    console.table(result);
+    return result;
+}
